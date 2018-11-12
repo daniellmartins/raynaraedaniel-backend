5a1e9e626f4f05de8208d70f5633df5bae78ffd5
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-12 14:45:58 -0200

    setting function isAuthenticated for subscriptions

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -3,7 +3,13 @@ import { verify } from "jsonwebtoken";
 import { APP_SECRET } from "../config";
 
 const isAuthenticated = async (resolve, _, args, ctx, info) => {
-  const authorized = ctx.request.get("Authorization");
+  let authorized;
+  if (info.operation.operation === "subscription") {
+    authorized = await ctx.connection.context.Authorization;
+  } else {
+    authorized = await ctx.request.get("Authorization");
+  }
+
   if (!authorized) throw new Error("Not authorized");
 
   const token = authorized.replace("Bearer ", "");
