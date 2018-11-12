5052c59bd8a0c1154334215cd03d7e9aa3ac46cb
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-11 21:32:43 -0300

    remove index file

--- src/api/Auth/index.js
+++
@@ -1,7 +0,0 @@
-import authResolvers from "./auth.resolvers";
-import { loadGQLFile } from "../../utils";
-
-export default {
-  resolvers: authResolvers,
-  typeDefs: loadGQLFile("Auth/auth.graphql")
-};
--- src/api/User/index.js
+++
@@ -1,7 +0,0 @@
-import userResolvers from "./user.resolvers";
-import { loadGQLFile } from "../../utils";
-
-export default {
-  resolvers: userResolvers,
-  typeDefs: loadGQLFile("User/user.graphql")
-};
