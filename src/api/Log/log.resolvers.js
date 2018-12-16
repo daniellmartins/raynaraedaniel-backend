72920f75ee1d459b61db06fcbe1dfee97f6254ea
Daniel Martins <daniellmarttins@hotmail.com>
2018-12-16 15:32:00 -0300

    add skip, limit and orderBy

--- src/api/Log/log.resolvers.js
+++ src/api/Log/log.resolvers.js
@@ -3,13 +3,20 @@ export default {
     log: async (_, { where }, { db }) => {
       return await db.log.findById(where._id);
     },
-    logs: async (_, args, { db }) => {
-      return await db.log.find().sort({ createdAt: -1 });
+    logs: async (
+      _,
+      { limit = 50, offset = 0, orderBy = { createdAt: -1 } },
+      { db }
+    ) => {
+      return await db.log
+        .find()
+        .skip(offset)
+        .limit(limit)
+        .sort(orderBy);
     }
   },
   Log: {
     user: async (_, args, { db }) => {
-      console.log(_);
       return await db.user.findById(_.userId);
     }
   }
